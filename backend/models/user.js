4b79d924cb8b183e35563041c7721cdaa0ac5eec
Priyanshi Nair <82303249+priyanshinair@users.noreply.github.com>
2023-08-06 11:18:20 +0530

    Update user.js

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,1 +1,44 @@
+import mongoose from "mongoose";
 
+const userSchema = new mongoose.Schema({
+    firstName:{
+        type:String,
+        required:true,
+        min: 2,
+        max: 50
+    },
+    lastName:{
+        type:String,
+        required:true,
+        min: 2,
+        max: 50
+    },
+    email:{
+        type:String,
+        required:true,
+        unique: true,
+        max: 50
+    },
+    password:{
+        type:String,
+        required:true,
+        min: 5,
+        max: 50
+    },
+    picturePath:{
+        type:String,
+        default:""
+    },
+    friends:{
+        type:Array,
+        default:[]
+    },
+    location: String,
+    occupation: String,
+    viewdProfile: Number,
+    imporessions: Number
+}, {timestamps: true}
+)
+
+const User = mongoose.model("User", userSchema)
+export default User;
